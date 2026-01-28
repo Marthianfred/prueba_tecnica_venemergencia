c336c08b355bbc824ab7a0d11a7c6c65d6a4ed29
Freddy Campos <marthianfred@gmail.com>
2026-01-28 18:40:32 -0400

    [#20]: Sec Fix Removed Hardcoded Secrets

--- src/shared/config/api.config.ts
+++ src/shared/config/api.config.ts
@@ -1,8 +1,11 @@
 export const API_CONFIG = {
   BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://api.themoviedb.org/3",
-  API_KEY:
-    process.env.NEXT_PUBLIC_TMDB_TOKEN || "3fd2be6f0c70a2a598f084ddfb75487c",
+  API_KEY: process.env.NEXT_PUBLIC_API_KEY || "",
   IMAGE_BASE_URL:
     process.env.NEXT_PUBLIC_IMAGE_BASE_URL || "https://image.tmdb.org/t/p",
   PLACEHOLDER_IMAGE: "https://via.placeholder.com/500x750?text=No+Image",
 };
+
+if (!API_CONFIG.API_KEY && typeof window !== "undefined") {
+  console.warn("⚠️ API KEY no configurada. Revisa tu archivo .env");
+}
